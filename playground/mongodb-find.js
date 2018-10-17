c63d70a004cf89e6c7c7a3af3a503775bfa81b2d
David Rachamim <david.rachamim@gmail.com>
2018-10-17 18:03:23 +0300

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,32 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/test/TodoApp', { useNewUrlParser: true }, (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5bc736b0dd226505692dee40')
+    // }).toArray().then(docs => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, err => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find().count().then(count => {
+    //     console.log(`Todos count: ${count}`);
+    // }, err => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Users').find({ name: 'David' }).toArray().then(docs => {
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, err => {
+        console.log('Unable to fetch todos', err);
+    });
+
+    client.close();
+});
