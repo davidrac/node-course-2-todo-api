5223ab965590beeecb465e395223be0e10502de5
David Rachamim <david.rachamim@gmail.com>
2018-10-17 18:37:01 +0300

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,31 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/test/TodoApp', { useNewUrlParser: true }, (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').deleteMany({ text: 'Eat lunch' }).then(result => {
+    //     console.log(result);
+    // });
+
+    // db.collection('Todos').deleteOne({ text: 'Eat lunch' }).then(result => {
+    //     console.log(result);
+    // });
+
+    // db.collection('Todos').findOneAndDelete({ completed: false }).then(result => {
+    //     console.log(result);
+    // });
+
+    // db.collection('Users').deleteMany({ name: 'David'} ).then(result => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndDelete({ _id: new ObjectID('5bc72dc3171bbdab7384d479') }).then(result => {
+        console.log(result);
+    });
+
+    client.close();
+});
